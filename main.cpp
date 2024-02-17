4e0aaba520601c26d1194da30a573ac01dc55d5b
Jurgis Jaška <jurgis.jaska@gmail.com>
2024-02-17 11:59:23 +0200

    Adds working hello world window for GTK

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include <gtk/gtk.h>
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
+void hello(GtkWidget *widget, gpointer data) {
+    g_print("Hello World\n");
+}
+
+int main(int argc, char *argv[]) {
+    GtkWidget *window;
+    GtkWidget *button;
+
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    button = gtk_button_new_with_label("Hello World");
+
+    g_signal_connect(button, "clicked", G_CALLBACK(hello), NULL);
+
+    gtk_container_add(GTK_CONTAINER(window), button);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
+
     return 0;
 }
